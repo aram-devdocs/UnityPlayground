118881334758ac91a08c68a4e4f66b7b652bebf5
aram-devdocs <aram.devdocs@gmail.com>
2020-02-09 13:56:58 -0700

    added vineGrab.cs

---
+++ Assets/Scripts/vineGrab.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vineGrab : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter()
+    {
+        Debug.Log("Trigggggered");
+        Debug.Log(rb.velocity);
+    }
+}
